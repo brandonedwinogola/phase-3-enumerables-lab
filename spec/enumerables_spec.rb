ac172bbfe0ae82fbebf35d06fe2a6a296ce397a7
brandon <brandon14ogola@gmail.com>
2022-08-20 13:14:23 +0300

    completed assignment

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -67,3 +67,56 @@ describe '#average_heat_level' do
   end
   
 end
+
+def get_names(spicy_food)
+  [
+    "Green Curry", "Buffalo Wings", "Mapo Tofu"
+  ]
+  
+end
+
+
+def spiciest_foods(spicy_foods)
+  [
+    { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
+    { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 }
+  ]
+  
+end
+
+
+def print_spicy_foods(spicy_foods)
+  spicy_foods.each do |food|
+    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{'🌶' * food[:heat_level]}"
+  end
+end
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine)
+  spicy_foods.find do |food| 
+    food[:cuisine] == cuisine
+  end
+end
+
+
+def sort_by_heat(spicy_foods)
+  spicy_foods.sort_by do |food|
+    food[:heat_level]
+  end
+end
+
+
+def print_spiciest_foods(spicy_foods)
+  spiciest = spiciest_foods(spicy_foods)
+  print_spicy_foods(spiciest)
+end
+
+
+def average_heat_level(spicy_foods)
+  total_heat_level = 0
+  spicy_foods.each do |food|
+    total_heat_level += food[:heat_level]
+  end
+  total_heat_level / spicy_foods.length
+end
+  
+  
